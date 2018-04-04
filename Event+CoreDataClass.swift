43e6755523b28759bfe4e5e67e0619dc3821ad2e
Student <student@CVS-R107D018.nic.bc.ca>
2018-04-03 18:04:40 -0700

    third commit

---
+++ Event+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Event+CoreDataClass.swift
+//  MyProjectEvent
+//
+//  Created by Student on 2018-04-03.
+//  Copyright © 2018 Student. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Event: NSManagedObject {
+
+}
---
+++ Event+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Event+CoreDataProperties.swift
+//  MyProjectEvent
+//
+//  Created by Student on 2018-04-03.
+//  Copyright © 2018 Student. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Event {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Event> {
+        return NSFetchRequest<Event>(entityName: "Event")
+    }
+
+    @NSManaged public var eventName: String?
+    @NSManaged public var eventLocation: String?
+    @NSManaged public var imageId: Int16
+    @NSManaged public var eventDiscription: String?
+    @NSManaged public var longitude: Double
+    @NSManaged public var latitude: Double
+    @NSManaged public var category: String?
+
+}
